e1bf49d3b94fb8b72563ee9ebc1221066b6648d5
ruberoed <robertkrasnyuk@gmail.com>
2022-09-09 16:34:56 +0300

    auth0909

---
+++ app.js
@@ -0,0 +1,122 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import { validationResult, body } from 'express-validator'
+import  registerValidation   from './validations/auth.js'
+import UserModel from  './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+
+
+
+mongoose
+    .connect('mongodb+srv://@projectcluster.mdfp4he.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB OK'))
+    .catch((err) => (console.log('PIZDA, NE RABOTAET !!!', err)))
+const app = express()
+
+app.use(express.json())
+
+app.post('/auth/login', async (req, res) => {
+    try{
+        const user = await UserModel.findOne({email: req.body.email})
+
+        if (!user){
+            return res.status(404).json({
+                message: 'User not found'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+        if (!isValidPass){
+            return res.status(400).json({
+                message: 'pass wrong'
+            })
+            console.log('pizdda')
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret123',
+            {
+            expiresIn: '30d',
+            },    
+            );
+
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json({
+        ...userData,
+        token,  
+        });
+    }catch(err){
+    console.log(err)
+    res.status(500).json({
+        message: 'не удалось авторизоваться'
+    })
+    }
+})
+
+
+app.post('/auth/register',  registerValidation, async (req, res) => {
+try{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json(errors.array())
+    }
+
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt) 
+
+    const doc = new UserModel({
+        email: req.body.email,
+        name: req.body.name,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: hash,
+    });
+
+
+    const user = await doc.save()
+
+    const token = jwt.sign(
+    {
+        _id: user._id,
+    },
+    'secret123',
+    {
+    expiresIn: '30d',
+    },    
+    );
+
+    const {passwordHash, ...userData} = user._doc;
+
+    res.json({
+        ...userData,
+        token,  
+    });
+} catch(err){
+    console.log(err)
+    res.status(500).json({
+        message: 'не удалось зарегестрироваться'
+    })
+}
+})
+
+app.get('/auth/me', checkAuth, (req, res) => {
+    try{
+
+    }catch(err){
+        
+    }
+})
+
+app.listen(1313, (err) => {
+    if (err) {
+        console.log('chtoto poshlo ne tak')
+    }
+
+    console.log('Rabotaet')
+});
