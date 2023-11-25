7e132c7acdb8c61359105ab45d43e526811b1497
roy <97613023+malvinaro@users.noreply.github.com>
2023-11-25 11:45:25 +0200

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,7 +21,7 @@ export const register = async (req, res) => {
             {
                 _id: user._id,
             },
-            'parashut',
+            'salt',
             {
                 expiresIn: '30d',
             },
